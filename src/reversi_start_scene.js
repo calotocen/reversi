1dafd0c5e5ba52ae84592fb79801166b38e73bcb
calotocen <calotocen@gmail.com>
2019-09-27 16:59:34 +0900

    手番選択がさなれていない場合の処理を追加した。

--- src/reversi_start_scene.js
+++ src/reversi_start_scene.js
@@ -6,11 +6,19 @@ class StartScene {
     start() {
         this.viewElement.style.display = "";
         window.onclick = event => {
+            // 選択した手番を取得して、これをチェックする。
             let move = event.target.getAttribute("value");
+            if (move != "black" && move != "white" && move != "random") {
+                // 手番選択がなされていないため、処理を中止する。
+                return;
+            }
+
+            // 手番選択が「おまかせ」である場合は、先手・後手を決める。
             if (move == "random") {
                 move = (Math.random() < 0.5) ? "black" : "white";
             }
 
+            // 本シーンで表示したビューを非表示化後、次のシーンを実行する。
             this.viewElement.style.display = "none";
             (new MainScene(move)).start();
         };
